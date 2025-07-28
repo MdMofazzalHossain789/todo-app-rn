bb4c8ad173cf971f4b402d9d591fff12f3c54b20
Md Mofazzal Hossain <mdmofazzalhossain789@gmail.com>
2025-07-28 09:44:51 +0600

    added convex server

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,17 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
-const index = () => {
+const Home = () => {
   const { toggleDarkMode } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAllTodo = useMutation(api.todos.deleteAllTodo);
 
   return (
     <View>
@@ -13,10 +21,24 @@ const index = () => {
           toggleDarkMode();
         }}
       >
-        Click to change theme
+        <Text>Click to change theme</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          addTodo({ text: "New Todo" });
+        }}
+      >
+        <Text>Add new Todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          clearAllTodo();
+        }}
+      >
+        <Text>Delete All Todos</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default index;
+export default Home;
